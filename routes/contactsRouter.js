ffc2c9074c45986f019ae829d4e1ba5a1a6ec4bc
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2024-02-11 16:02:06 +0200

    ...

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const contacts = require("../controllers/contactsControllers.js");
 const validateBody = require("../helpers/validateBody");
 const sсhema = require("../schemas/contactsSchemas");
-
+const jwtMiddleware = require("../middleware/jwtMiddleware");
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contacts.getAllContacts);
@@ -13,18 +13,22 @@ contactsRouter.delete("/:id", contacts.deleteContact);
 
 contactsRouter.post(
   "/",
+  jwtMiddleware,
   validateBody(sсhema.createContactSchema),
+
   contacts.createContact
 );
 
 contactsRouter.put(
   "/:id",
+  jwtMiddleware,
   validateBody(sсhema.updateContactSchema),
   contacts.updateContact
 );
 
 contactsRouter.patch(
   "/:id/favorite",
+  jwtMiddleware,
   validateBody(sсhema.updatefavoriteSchema),
   contacts.updateFavorite
 );
