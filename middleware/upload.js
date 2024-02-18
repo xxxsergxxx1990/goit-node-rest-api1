feef4e4196e3930391e00f9ceda187e4ef456370
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2024-02-18 17:28:10 +0200

    \\

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,9 +6,12 @@ const tempDir = path.join(__dirname, "../", "tmp");
 const multerConfig = multer.diskStorage({
   destination: tempDir,
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const extension = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + extension);
   },
 });
+
 
 const upload = multer({
   storage: multerConfig,
